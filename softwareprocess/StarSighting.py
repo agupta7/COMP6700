b51996ac7732031361f5b000a7a1367b7bc495cb
Anuj Gupta <azg0076@auburn.edu>
2017-03-22 18:40:01 -0500

    Green Light |

--- softwareprocess/StarSighting.py
+++ softwareprocess/StarSighting.py
@@ -4,3 +4,9 @@ class StarSighting(object):
     def __init__(self, degrees, minutes):
         self._degrees = degrees
         self._minutes = minutes
+
+    def getDegrees(self):
+        return self._degrees
+
+    def getMintues(self):
+        return self._minutes
