9790727eae6c5363f23c3fcd4cb67855478e9d7f
Anuj Gupta <azg0076@auburn.edu>
2017-03-22 19:05:27 -0500

    Red Light |

--- softwareprocess/StarSighting.py
+++ softwareprocess/StarSighting.py
@@ -5,6 +5,8 @@ class StarSighting(object):
         strDegreesBound = "Degrees should be an integer >= 0 and < 90."
         if not isinstance(degrees, (int, long)):
             raise ValueError(strDegreesBound)
+        elif degrees < 0:
+            raise ValueError(strDegreesBound)
 
         self._degrees = degrees
         self._minutes = minutes
