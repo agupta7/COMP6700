ac0f56ef4184e99b6c9d8f176f4613f935ef357d
Anuj Gupta <azg0076@auburn.edu>
2017-03-22 19:11:08 -0500

    Green Light |

--- softwareprocess/StarSighting.py
+++ softwareprocess/StarSighting.py
@@ -3,12 +3,16 @@ import math
 class StarSighting(object):
     def __init__(self, degrees, minutes):
         strDegreesBound = "Degrees should be an integer >= 0 and < 90."
+        strMinutesBound = "Minutes should be a decimal >= 0.0 and < 60.0."
         if not isinstance(degrees, (int, long)):
             raise ValueError(strDegreesBound)
         elif degrees < 0:
             raise ValueError(strDegreesBound)
         elif degrees >= 90:
             raise ValueError(strDegreesBound)
+
+        if not isinstance(degrees, (int, long, float)):
+            raise ValueError(strMinutesBound)
 
         self._degrees = degrees
         self._minutes = minutes
