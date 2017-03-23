9e421cab57cc41d193724f65dcf4b9dc679a7105
Anuj Gupta <azg0076@auburn.edu>
2017-03-22 19:04:40 -0500

    Green Light |

--- softwareprocess/test/StarSightingTest.py
+++ softwareprocess/test/StarSightingTest.py
@@ -39,25 +39,21 @@ class StarSightingTest(unittest.TestCase):
         self.assertIsInstance(ss, SS.StarSighting)
         self.assertEquals(ss.getDegrees(), 20)
         self.assertEquals(ss.getMinutes(), 30.0)
-
     def test100_020_ShouldConstructLowDegrees(self):
         ss = SS.StarSighting(0, 30.0)
         self.assertIsInstance(ss, SS.StarSighting)
         self.assertEquals(ss.getDegrees(), 0)
         self.assertEquals(ss.getMinutes(), 30.0)
-
     def test100_030_ShouldConstructHighDegrees(self):
         ss = SS.StarSighting(89, 30.0)
         self.assertIsInstance(ss, SS.StarSighting)
         self.assertEquals(ss.getDegrees(), 89)
         self.assertEquals(ss.getMinutes(), 30.0)
-
     def test100_040_ShouldConstructLowMinutes(self):
         ss = SS.StarSighting(20, 0.0)
         self.assertIsInstance(ss, SS.StarSighting)
         self.assertEquals(ss.getDegrees(), 20)
         self.assertEquals(ss.getMinutes(), 0.0)
-
     def test100_050_ShouldConstructHighMinutes(self):
         ss = SS.StarSighting(20, 59.9)
         self.assertIsInstance(ss, SS.StarSighting)
@@ -68,15 +64,17 @@ class StarSightingTest(unittest.TestCase):
         with self.assertRaises(ValueError) as cxt:
             ss = SS.StarSighting(20.5, 30.0)
         self.assertEquals(cxt.exception.args[0], self.strDegreesBound)
-
     def test100_810_ShouldRaiseExceptionNullDegrees(self):
         with self.assertRaises(ValueError) as cxt:
             ss = SS.StarSighting(None, 30.0)
         self.assertEquals(cxt.exception.args[0], self.strDegreesBound)
-
     def test100_820_ShouldRaiseExceptionWrongTypeDegrees(self):
         with self.assertRaises(ValueError) as cxt:
             ss = SS.StarSighting('a', 30.0)
+        self.assertEquals(cxt.exception.args[0], self.strDegreesBound)
+    def test100_830_ShouldRaiseExceptionDegreesLow(self):
+        with self.assertRaises(ValueError) as cxt:
+            ss = SS.StarSighting(-1, 30.0)
         self.assertEquals(cxt.exception.args[0], self.strDegreesBound)
 # ---- Unit tests
 # ----
