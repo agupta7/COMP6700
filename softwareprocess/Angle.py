106040911e8edcfc66560e3f7119ae0a212983e2
Anuj Gupta <azg0076@auburn.edu>
2017-04-07 18:21:55 -0500

    Green Light |

--- softwareprocess/Angle.py
+++ softwareprocess/Angle.py
@@ -1,3 +1,35 @@
 
 class Angle:
-    pass
+
+    def __init__(self, degStr):
+        strDegreesMinutesFormatError = "String should in format XdY.Y where X is degrees and Y.Y is floating point minutes"
+
+        if not isinstance(degStr, (str, basestring)):
+            raise ValueError(strDegreesMinutesFormatError)
+        pieces = degStr.split('d')
+
+        if len(pieces) != 2:
+            raise ValueError(strDegreesMinutesFormatError)
+        try:
+            degrees = int(pieces[0])
+            minutes = float(pieces[1])
+            self._degrees = degrees
+            self._minutes = minutes
+        except ValueError as er:
+            raise ValueError(strDegreesMinutesFormatError)
+
+    def getDegrees(self):
+        return self._degrees
+    def setDegrees(self, degrees):
+        if not isinstance((int, long)):
+            raise ValueError("Degrees should be an integer")
+
+        self._degrees = degrees
+
+    def getMinutes(self):
+        return self._minutes
+    def setMinutes(self, minutes):
+        if not isinstance((int, long, float)):
+            raise ValueError("Minutes should be a float")
+
+        self._minutes = minutes
