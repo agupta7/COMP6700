fdfdc808bc8bb9c901b55a788db4f44091e7282c
Anuj Gupta <azg0076@auburn.edu>
2017-04-26 17:59:08 -0500

    Green Light |

--- softwareprocess/test/SightingErrorCorrectorTest.py
+++ softwareprocess/test/SightingErrorCorrectorTest.py
@@ -40,7 +40,6 @@ class SightingErrorCorrectorTest(unittest.TestCase):
 
         sec = SEC.SightingErrorCorrector(lat=lat, long=long, altitude=alt, assumedLat=assumedLat, assumedLong=assumedLong)
         self.assertIsInstance(sec, SEC.SightingErrorCorrector)
-
     def test100_020_ShouldConstructHighInputs(self):
         lat = '89d59.9'
         long = '359d59.9'
@@ -50,7 +49,6 @@ class SightingErrorCorrectorTest(unittest.TestCase):
 
         sec = SEC.SightingErrorCorrector(lat=lat, long=long, altitude=alt, assumedLat=assumedLat, assumedLong=assumedLong)
         self.assertIsInstance(sec, SEC.SightingErrorCorrector)
-
     def test100_030_ShouldConstructLowInputs(self):
         lat = '-89d59.9'
         long = '0d0.0'
@@ -60,3 +58,16 @@ class SightingErrorCorrectorTest(unittest.TestCase):
 
         sec = SEC.SightingErrorCorrector(lat=lat, long=long, altitude=alt, assumedLat=assumedLat, assumedLong=assumedLong)
         self.assertIsInstance(sec, SEC.SightingErrorCorrector)
+
+    def test100_910_ShouldRaiseValueErrorLatTooHigh(self):
+        lat = '90d0.0'
+        long = '95d41.6'
+        alt = '13d42.3'
+        assumedLat = '-53d38.4'
+        assumedLong = '74d35.3'
+
+        with self.assertRaises(ValueError) as ctx:
+            sec = SEC.SightingErrorCorrector(lat=lat, long=long, altitude=alt, assumedLat=assumedLat, assumedLong=assumedLong)
+        self.assertIsInstance(ctx.exception.args[0], (str, basestring))
+
+    
