601b67de440d1ba49c1442034d0ecad9a803562d
Anuj Gupta <azg0076@auburn.edu>
2017-04-26 16:18:15 -0500

    Green Light |

--- softwareprocess/angles/Latitude.py
+++ softwareprocess/angles/Latitude.py
@@ -1,4 +1,11 @@
 import softwareprocess.Angle as A
 
 class Latitude(A.Angle):
-    pass
+    def __init__(self, degreesStrOrFloat):
+        latitudeTooLowStr = 'The latitude specified is too low!  It must be greater than -90 degrees and 0.0 minutes'
+        latitudeTooHighStr = 'The latitude specified is too high!  It must be less than 90 degrees and 0.0 minutes'
+        A.Angle.__init__(self, degreesStrOrFloat)
+        if self._degreesFloat <= -90:
+            raise ValueError(latitudeTooLowStr)
+        elif self._degreesFloat >= 90:
+            raise ValueError(latitudeTooHighStr)
