821c845a258849366092d9b5a3219f7e48345b62
Anuj Gupta <azg0076@auburn.edu>
2017-04-26 18:01:57 -0500

    Red Light |

--- softwareprocess/SightingErrorCorrector.py
+++ softwareprocess/SightingErrorCorrector.py
@@ -1,4 +1,11 @@
+import softwareprocess.angles.Latitude as Lat
+import softwareprocess.angles.Altitude as Alt
+import softwareprocess.angles.Longitude as Long
 
 class SightingErrorCorrector:
     def __init__(self, lat, long, altitude, assumedLat, assumedLong):
-        pass
+        self._lat = Lat.Latitude(lat)
+        self._long = Long.Longitude(long)
+        self._altitude = Alt.Altitude(altitude)
+        self._assumedLat = Lat.Latitude(assumedLat)
+        self._assumedLong = Long.Longitude(assumedLong)
