ae429f60d9776a5f7ebc173579184310c71a8999
Anuj Gupta <azg0076@auburn.edu>
2017-04-26 15:46:12 -0500

    Green Light |

--- softwareprocess/angles/Altitude.py
+++ softwareprocess/angles/Altitude.py
@@ -3,7 +3,6 @@ import softwareprocess.Angle as A
 class Altitude(A.Angle):
     def __init__(self, degreesStrOrFloat):
         strAngleTooLowException = 'The altitude specified is too low!  It must be greater than 0 degrees and 0.0 minutes'
-        super(Altitude)
         A.Angle.__init__(self, degreesStrOrFloat)
         if self._degreesFloat <= 0:
             raise ValueError(strAngleTooLowException)
